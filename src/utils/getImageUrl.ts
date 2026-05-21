b29518035f63360a957e8de8509bc375ab80aca0
Yead191 <yead191@gmail.com>
2026-05-21 09:40:11 +0600

    updated

--- src/utils/getImageUrl.ts
+++ src/utils/getImageUrl.ts
@@ -1,5 +1,5 @@
 export function getImageUrl(imageurl: string) {
-  const imageUrl = import.meta.env.VITE_API_BASE_URL; 
+  const imageUrl = import.meta.env.VITE_API_BASE_URL;
   if (imageurl?.startsWith("http") || imageurl?.startsWith("blob:"))
     return imageurl;
   return imageUrl + imageurl;
